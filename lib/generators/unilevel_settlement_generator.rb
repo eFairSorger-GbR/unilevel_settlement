42b6c2c04004e4c2c5337c79238a44d7d31bd7ad
Kevin Liebholz <liebholz@crowddesk.de>
2020-12-20 17:57:03 +0100

    FEAT: add initilizer generator

---
+++ lib/generators/unilevel_settlement_generator.rb
@@ -0,0 +1,16 @@
+class UnilevelSettlementGenerator < Rails::Generators::Base
+  desc "Description: Create the customizable config file for UnilevelSettlement"
+
+  def create_initializer_file
+    create_file "config/initializers/unilevel_settlement.rb", content
+  end
+
+  def content
+    <<~EOS
+      # This file can be used to adapt the unilevel_settlement gem to your app!
+
+      # Set the class from your app that will receive the settlement (e.g. User, Consultant, Agent)
+      UnilevelSettlement.user_class = 'User'
+    EOS
+  end
+end
