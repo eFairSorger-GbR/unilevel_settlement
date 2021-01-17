7ca5640cf6315accdbd5816525df06fec99bf5d4
Kevin Liebholz <liebholz@crowddesk.de>
2021-01-17 16:08:43 +0100

    FIX: js

--- app/assets/config/unilevel_settlement_manifest.js
+++ app/assets/config/unilevel_settlement_manifest.js
@@ -1,1 +1,2 @@
 //= link_directory ../stylesheets/unilevel_settlement .css
+//= link_directory ../javascripts/unilevel_settlement .js
--- app/models/concerns/unilevel_settlement/level_uniqeness_validator.rb
+++ app/models/concerns/unilevel_settlement/level_uniqeness_validator.rb
@@ -2,7 +2,7 @@ class UnilevelSettlement::LevelUniqenessValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, _value)
     return unless record.attribute.present?
 
-    levels_arr = record.public_send(attribute).map { |i| i.provision.level }
+    levels_arr = record.send(attribute).map { |i| i.provisions.map(&:level) }
     record.errors.add(attribute, 'das Level wurde bereits vergeben') unless levels_arr.count == levels_arr.uniq.count
   end
 end
