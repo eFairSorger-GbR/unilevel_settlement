90d65034225d7bf009b990cc138d09ecd98f97cf
Kevin Liebholz <liebholz@crowddesk.de>
2021-01-18 22:22:21 +0100

    WIP: associations

--- app/models/unilevel_settlement/provider.rb
+++ app/models/unilevel_settlement/provider.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
 module UnilevelSettlement
   class Provider < ApplicationRecord
     include UnilevelSettlement::DelegateAttributes
 
-    belongs_to :provisions_template, optional: true
-    has_many :provisions
-    has_many :contracts
+    belongs_to :provisions_template, class_name: 'UnilevelSettlement::ProvisionsTemplate',
+                                     foreign_key: 'unilevel_settlement_provisions_template_id', optional: true
+    has_many :provisions, class_name: 'UnilevelSettlement::Provision'
+    has_many :contracts, class_name: 'UnilevelSettlement::Contract', foreign_key: 'unilevel_settlement_contract_id'
 
     accepts_nested_attributes_for :provisions, reject_if: :all_blank, allow_destroy: true
 
--- app/models/unilevel_settlement/provision.rb
+++ app/models/unilevel_settlement/provision.rb
@@ -1,25 +1,29 @@
+# frozen_string_literal: true
+
 module UnilevelSettlement
   class Provision < ApplicationRecord
-    belongs_to :unilevel_settlement_provider, optional: true
-    belongs_to :unilevel_settlement_provisions_template, optional: true
+    belongs_to :provider, class_name: 'UnilevelSettlement::Provider',
+                          foreign_key: 'unilevel_settlement_provider_id', optional: true
+    belongs_to :provisions_template, class_name: 'UnilevelSettlement::ProvisionsTemplate',
+                                     foreign_key: 'unilevel_settlment_provisions_template_id', optional: true
 
     validates :level, presence: true
     validates :provision, presence: true, numericality: { greater_thatn_or_equal_to: 0 }
-    validates :unilevel_settlement_provider, 'unilevel_settlement/level_uniqueness': true
-    validates :unilevel_settlement_provisions_template, 'unilevel_settlement/level_uniqueness': true
+    validates :provider, 'unilevel_settlement/level_uniqueness': true
+    validates :provisions_template, 'unilevel_settlement/level_uniqueness': true
 
     validate :provider_or_template_must_exist, :only_one_reference_assigned
 
     private
 
     def provider_or_template_must_exist
-      return if unilevel_settlement_provider || unilevel_settlement_provisions_template
+      return if provider || provisions_template
 
       errors.add(:provider, 'Anbieter oder Provisions Template muss ausgefüllt sein')
     end
 
     def only_one_reference_assigned
-      return unless unilevel_settlement_provider && unilevel_settlement_provisions_template
+      return unless provider && provisions_template
 
       errors.add(:provider, 'Nur Anbieter oder Provisions Template darf ausgefüllt sein')
     end
