557c2ecf703ac063d1f752b3446171f692855f90
Kevin Liebholz <liebholz@crowddesk.de>
2021-01-20 21:09:34 +0100

    FIX-WIP: level_uniqueness validator

--- app/models/unilevel_settlement/provision.rb
+++ app/models/unilevel_settlement/provision.rb
@@ -10,8 +10,10 @@ module UnilevelSettlement
 
     validates :level, presence: true
     validates :provision, presence: true, numericality: { greater_thatn_or_equal_to: 0 }
-    validates :provider, 'unilevel_settlement/level_uniqueness': true
-    validates :provisions_template, 'unilevel_settlement/level_uniqueness': true
+
+    #### --> Issue is here!!!!
+    # validates :provider, 'unilevel_settlement/level_uniqueness': true
+    # validates :provisions_template, 'unilevel_settlement/level_uniqueness': true
 
     validate :provider_or_template_must_exist, :only_one_reference_assigned
 
