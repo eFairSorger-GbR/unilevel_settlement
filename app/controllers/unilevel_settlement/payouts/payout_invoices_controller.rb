01b90ccc96867a9273cf2d196521a9204475885d
Kevin Liebholz <liebholz@crowddesk.de>
2021-02-02 20:31:37 +0100

    FEAT: cancel payout run if users are missing in DB

--- app/controllers/unilevel_settlement/payouts/payout_invoices_controller.rb
+++ app/controllers/unilevel_settlement/payouts/payout_invoices_controller.rb
@@ -6,7 +6,14 @@ module UnilevelSettlement
       def create_all
         excel_reader = PayoutRunExcelReader.new(@payout_run)
         generator = PayoutInvoicesGenerator.new(@payout_run, excel_reader)
-        generator.all_users_known?
+
+        if generator.all_users_known?
+          ''
+        else
+          @payout_run.cancel
+          flash[:error] = 'Nicht alle Berater aus der Excel existieren in der Datenbank. Die Abrechnung wurde abgebrochen und alle dazugehörigen Daten wurden gelöscht.'
+          redirect_to payouts_payout_runs_path
+        end
       end
     end
   end
