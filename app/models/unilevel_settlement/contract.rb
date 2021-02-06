e830a10810cb02f1e9a40956ca23b9f859684787
Kevin Liebholz <liebholz@crowddesk.de>
2021-02-06 13:50:38 +0100

    [FEAT] add user relation to invoice

--- app/models/unilevel_settlement/contract.rb
+++ app/models/unilevel_settlement/contract.rb
@@ -2,7 +2,7 @@ module UnilevelSettlement
   class Contract < ApplicationRecord
     belongs_to :provider, class_name: 'UnilevelSettlement::Provider', foreign_key: 'unilevel_settlement_provider_id',
                           optional: true, inverse_of: :contracts
-    belongs_to :user, class_name: UnilevelSettlement.user_class.to_s, foreign_key: 'user_id'
+    belongs_to :user, class_name: UnilevelSettlement.user_class.to_s, foreign_key: 'user_id', optional: false, inverse_of: :contracts
 
     has_many :records, class_name: 'UnilevelSettlement::PayoutRecord', foreign_key: 'unilevel_settlement_contract_id',
                        inverse_of: :contract
--- app/models/unilevel_settlement/payout_invoice.rb
+++ app/models/unilevel_settlement/payout_invoice.rb
@@ -2,7 +2,8 @@ module UnilevelSettlement
   class PayoutInvoice < ApplicationRecord
     belongs_to :run, class_name: 'UnilevelSettlement::PayoutRun', foreign_key: 'unilevel_settlement_payout_run_id',
                      optional: false, inverse_of: :invoices
-    belongs_to :user, class: UnilevelSettlement.user_class.to_s
+    belongs_to :user, class_name: UnilevelSettlement.user_class.to_s, foreign_key: 'user_id', optional: false,
+                      inverse_of: :invoices
 
     has_many :records, class_name: 'UnilevelSettlement::PayoutRecord', foreign_key: 'unilevel_settlement_payout_invoice_id',
                        inverse_of: :invoice
