d3bc51b837e5108025d594b6bd42401539626d6b
Kevin Liebholz <liebholz@crowddesk.de>
2021-05-19 17:19:32 +0200

    [FEAT] add follow_up to provisions_templates creation process

--- app/models/concerns/unilevel_settlement/level_order_validator.rb
+++ app/models/concerns/unilevel_settlement/level_order_validator.rb
@@ -2,11 +2,30 @@ class UnilevelSettlement::LevelOrderValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.empty?
 
-    levels = value.map(&:level)
-    record.errors.add(attribute, 'Level 0 als `Eigenprovision` muss angegeben sein') unless levels.include?(0)
+    normal_levels = value.reject(&:follow_up?).map(&:level)
+    follow_up_levels = value.select(&:follow_up?).map(&:level)
 
-    return if levels.sort == (levels.min..levels.max).to_a
+    level_0_missing_validation(record, attribute, normal_levels)
+    seamless_order_validation(record, attribute, normal_levels, follow_up_levels)
+  end
+
+  private
+
+  def level_0_missing_validation(record, attribute, normal_levels)
+    return if normal_levels.include?(0)
+
+    record.errors.add(attribute, 'Level 0 als `Eigenprovision` muss angegeben sein')
+  end
+
+  def seamless_order_validation(record, attribute, normal_levels, follow_up_levels)
+    return if seamless_order?(normal_levels) && seamless_order?(follow_up_levels)
 
     record.errors.add(attribute, 'Alle Level müssen durchgängig ohne Lücke sein')
+  end
+
+  def seamless_order?(levels)
+    return true if levels.empty?
+
+    levels.sort == (levels.min..levels.max).to_a
   end
 end
